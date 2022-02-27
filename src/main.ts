8eaebad8c317b2466524d5cee8a2a6c6bf98a318
israel leon <israel.leon@ioet.com>
2022-02-27 03:13:31 -0500

    feat : add mvc structure and hbs template engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import  helmet from 'helmet';
+import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet());
+
+  // static and view engines
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  
+  // config swagger 
+  const config = new DocumentBuilder()
+    .setTitle('RMB APP')
+    .setDescription('API de la ruta turística de la Mancomunidad Bosque Seco')
+    .setVersion('1.0')
+    .addTag('V1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
