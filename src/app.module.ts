af63fb43099511f69502159c9f8c5247f6492495
israel leon <israel.leon@ioet.com>
2022-02-27 23:06:20 -0500

    fix: 🐛 issues imports con app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { SharedModule } from '../config/config.module';
+import { SharedModule } from './config/config.module';
 
 @Module({
   imports: [
